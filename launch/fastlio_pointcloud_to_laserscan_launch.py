cc01d96346a60ffd65c2f3fb5a669c6f78163681
Jinxin Liu <liujinxin1996@gmail.com>
2025-07-14 22:04:55 +0800

    change local_map to pc2 to ensure safety

--- launch/fastlio_pointcloud_to_laserscan_launch.py
+++ launch/fastlio_pointcloud_to_laserscan_launch.py
@@ -13,7 +13,7 @@ def generate_launch_description():
         ),
         Node(
             package='pointcloud_to_laserscan', executable='pointcloud_to_laserscan_node',
-            remappings=[('cloud_in',  ['/local_map']),
+            remappings=[('cloud_in',  ['/livox/points']),
                         ('scan',  ['/scan'])],
             parameters=[{
                 'target_frame': 'lidar_link',
