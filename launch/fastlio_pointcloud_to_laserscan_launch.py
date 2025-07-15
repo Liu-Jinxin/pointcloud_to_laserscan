919c69a66e18ab3173d1795793ee711668d4c7f8
Jinxin Liu <liujinxin1996@gmail.com>
2025-07-15 19:00:23 +0800

    update topic

--- launch/fastlio_pointcloud_to_laserscan_launch.py
+++ launch/fastlio_pointcloud_to_laserscan_launch.py
@@ -13,21 +13,22 @@ def generate_launch_description():
         ),
         Node(
             package='pointcloud_to_laserscan', executable='pointcloud_to_laserscan_node',
-            remappings=[('cloud_in',  ['/livox/points']),
+            remappings=[('cloud_in',  ['/livox/lidar/pcd2']),
                         ('scan',  ['/scan'])],
             parameters=[{
-                'target_frame': 'lidar_link',
-                'transform_tolerance': 0.01,
+                'target_frame': 'livox_frame',
+                'transform_tolerance': 0.1,
                 'min_height': -0.5,
                 'max_height': 1.0,
-                'angle_min': -3.14159,  # -M_PI/2
-                'angle_max': 3.14159,  # M_PI/2
+                'angle_min': -3.14159,  # -M_PI
+                'angle_max': 3.14159,   # M_PI
                 'angle_increment': 0.003141592,
-                'scan_time': 0.3333,
+                'scan_time': 0.033,
                 'range_min': 0.45,
                 'range_max': 40.0,
                 'use_inf': True,
-                'inf_epsilon': 1.0
+                'inf_epsilon': 1.0,
+                'queue_size': 100
             }],
             name='pointcloud_to_laserscan'
         )
